808b0ba5921f4be09dfd1bff8959fcc0ff095235
toatall <toatall.seek@gmail.com>
2024-07-22 14:16:54 +0500

    Установлен PrimeVue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 import '../css/app.css'
+import 'primeicons/primeicons.css'
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import PrimeVue from 'primevue/config'
+import Lara from '@/presets/lara'
+import Aura from '@/presets/aura'
+import ConfirmationService from 'primevue/confirmationservice'
+import ToastService from 'primevue/toastservice'
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
@@ -8,6 +14,16 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .use(ConfirmationService)
+      .use(ToastService)
+      .use(PrimeVue, {
+        unstyled: true,
+        pt: Aura,
+        locale: {
+          accept: 'OK',
+          reject: 'Отмена',           
+        }
+      })
       .mount(el)
   },
 })
