ec4f18bd85588247d1211b868b4959fe7aea0ba4
toatall <toatall.seek@gmail.com>
2024-07-22 14:29:36 +0500

    Frontend: обновление шаблона

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -5,6 +5,9 @@ import Logo from '@/Shared/Logo.vue'
 import Dropdown from '@/Shared/Dropdown.vue'
 import MainMenu from '@/Shared/MainMenu.vue'
 import FlashMessages from '@/Shared/FlashMessages.vue'
+import { defineProps } from 'vue'
+import ConfirmDialog from 'primevue/confirmdialog'
+import Toast from 'primevue/toast'
 
 const props = defineProps({
     auth: Object,
@@ -32,13 +35,13 @@ const props = defineProps({
             </dropdown>
           </div>
           <div class="md:text-md flex items-center justify-between p-4 w-full text-sm bg-white border-b md:px-12 md:py-0">
-            <div class="mr-4 mt-1">{{ auth.user.account.name }}</div>
+            <div class="mr-4 mt-1">{{ `auth.user.account.name` }}</div>
             <dropdown class="mt-1" placement="bottom-end">
               <template #default>
                 <div class="group flex items-center cursor-pointer select-none">
                   <div class="mr-1 text-gray-700 group-hover:text-indigo-600 focus:text-indigo-600 whitespace-nowrap">
                     <span>{{ auth.user.first_name }}</span>
-                    <span class="hidden md:inline">&nbsp;{{ auth.user.last_name }}</span>
+                    <span class="hidden md:inline">&nbsp;{{ auth.user.full_name ?? auth.user.username }}</span>
                   </div>
                   <icon class="w-5 h-5 fill-gray-700 group-hover:fill-indigo-600 focus:fill-indigo-600" name="cheveron-down" />
                 </div>
@@ -63,4 +66,6 @@ const props = defineProps({
       </div>
     </div>
   </div>
+  <ConfirmDialog></ConfirmDialog>
+  <Toast />
 </template>
