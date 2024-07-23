ace7c1d7ed270db6ddfce734cfa3f12fd366746d
toatall <toatall.seek@gmail.com>
2024-07-23 14:56:44 +0500

    Добавлен выпадающий список

---
+++ resources/js/Shared/DropdownInput.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { v4 as uuid } from 'uuid'
+import Dropdown from 'primevue/dropdown'
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const props = defineProps({
+  id: {
+    type: String,
+    default() {
+      return `dropdown-input-${uuid()}`
+    },
+  },
+  error: String,
+  label: String,  
+})
+</script>
+
+<template>
+  <div :class="$attrs.class">
+    <label v-if="label" class="form-label" :for="id">{{ label }}:</label>
+    <Dropdown 
+      :id="id" 
+      v-bind="{ ...$attrs, class: null }" 
+      :invalid="error != undefined"
+      class="w-full" />    
+    <div v-if="error" class="form-error">{{ error }}</div>
+  </div>
+</template>
