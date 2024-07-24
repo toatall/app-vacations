4b87d64dc4ed7129886b76e980995edd59bf7096
toatall <toatall.seek@gmail.com>
2024-07-24 15:43:34 +0500

    Элемент получаемый при импорте данных

---
+++ models/VacationItem.php
@@ -0,0 +1,70 @@
+<?php
+namespace app\models;
+use yii\base\BaseObject;
+
+/**
+ * Элемент коллекции
+ * 
+ * @property string $dateStartUTC
+ * @property string $dateEndUTC
+ */
+class VacationItem extends BaseObject
+{
+    /**
+     * Дата начала
+     * @var string
+     */
+    public $dateStart;
+
+    /**
+     * Дата окончания
+     * @var string
+     */
+    public $dateEnd;
+
+    /**
+     * ФИО сотрудника
+     * @var string
+     */
+    public $fullName;
+
+    /**
+     * Код организации
+     * @var string
+     */
+    public $organization;
+
+    /**
+     * Наименование отдела
+     * @var string
+     */
+    public $department;
+
+    /**
+     * Вид отпуска
+     * @var string
+     */
+    public $kindVacation;
+
+    /**
+     * Статус
+     * @var string
+     */
+    public $status;  
+
+    public function getDateStartUTC()
+    {
+        return $this->dateToUTC($this->dateStart);
+    }
+
+    public function getDateEndUTC()
+    {
+        return $this->dateToUTC($this->dateEnd);
+    }
+
+    private function dateToUTC($value)
+    {
+        return \Yii::$app->formatter->asDate($value, 'Y-MM-dd');
+    }
+
+}
