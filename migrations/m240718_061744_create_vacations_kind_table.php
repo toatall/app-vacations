2453071f22179e2cdae981c4fc3bebae62d3d1ff
toatall <toatall.seek@gmail.com>
2024-07-30 09:03:28 +0500

    Изменена миграция таблицы vacations_kind. Удален столбец update_hash.

--- migrations/m240718_061744_create_vacations_kind_table.php
+++ migrations/m240718_061744_create_vacations_kind_table.php
@@ -16,12 +16,11 @@ class m240718_061744_create_vacations_kind_table extends Migration
         $this->createTable('vacations_kind', [
             'id' => $this->primaryKey(),
             'org_code' => $this->string(5)->notNull(),
-            'name' => $this->string(150)->notNull(),
+            'name' => $this->string(150)->notNull(),            
             'created_at' => $this->dateTime()->notNull()->defaultValue(new Expression('NOW()')),
-            'updated_at' => $this->dateTime()->notNull()->defaultValue(new Expression('NOW()')),
-            'update_hash' => $this->string(32)->notNull(),
+            'updated_at' => $this->dateTime()->notNull()->defaultValue(new Expression('NOW()')),            
         ]);
-        $this->addForeignKey('fk__vacations_kind__org_code', 'vacations_kind', 'org_code', 'organizations', 'code', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk__vacations_kind__org_code', 'vacations_kind', 'org_code', 'organizations', 'code', 'CASCADE', 'CASCADE');       
         $this->createIndex('index__vacations_kind__org_code__name', 'vacations_kind', ['org_code', 'name'], true);
     }
 
