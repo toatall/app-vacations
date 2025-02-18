2572d650fd572556844ad7708d10a5f70202a00c
taotall <toatall@mail.ru>
2025-02-18 16:41:10 +0500

    Скрипт обновления БД

---
+++ refresh.bat
@@ -0,0 +1,4 @@
+php yii migrate/down --interactive=false 8
+php yii migrate/down --migrationPath=@yii/rbac/migrations --interactive=false 4
+php yii migrate --migrationPath=@yii/rbac/migrations --interactive=false
+php yii migrate --interactive=false
