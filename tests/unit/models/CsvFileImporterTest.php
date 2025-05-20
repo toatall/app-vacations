432431fdb6c27e68435dd9c5b22b8c32ea29bb47
toatall <toatall.seek@gmail.com>
2025-05-20 09:50:57 +0500

    Исправлены тесты для проверки загрузки csv файлов

--- tests/unit/models/CsvFileImporterTest.php
+++ tests/unit/models/CsvFileImporterTest.php
@@ -3,11 +3,11 @@
 namespace tests\unit\models;
 
 use app\fixtures\OrganizationFixture;
-use app\models\collections\SimpleVacationCollection;
+use app\models\collections\FileVacationCollection;
 use app\models\importers\CsvFileImporter;
 
 class CsvFileImporterTest extends \Codeception\Test\Unit
-{    
+{
     public function _fixtures()
     {
         return [
@@ -17,15 +17,15 @@ class CsvFileImporterTest extends \Codeception\Test\Unit
             ]
         ];
     }
-    
+
     public function testLoad()
     {
         $codeOrg = '8600';
-        $year = '2024';
+        $year = date('Y');
         $csvFile = \Yii::getAlias('@app/tests/_files/import-data.csv');
-        $csvFileImporter = new CsvFileImporter($csvFile, new SimpleVacationCollection($codeOrg), $codeOrg, $year);
+        $csvFileImporter = new CsvFileImporter($csvFile, new FileVacationCollection($codeOrg), $codeOrg, $year);
         $csvFileImporter->run();
-        
+
         $resQuery = \Yii::$app->db->createCommand('SELECT * FROM "vacations"')->queryAll();
         $this->assertCount(10, $resQuery);
     }
@@ -33,14 +33,13 @@ class CsvFileImporterTest extends \Codeception\Test\Unit
     public function testLoadBadFileLong()
     {
         $codeOrg = '8600';
-        $year = '2024';
+        $year = date('Y');
         $csvFile = \Yii::getAlias('@app/tests/_files/import-data-bad-long.csv');
-        $csvFileImporter = new CsvFileImporter($csvFile, new SimpleVacationCollection($codeOrg), $codeOrg, $year);
+        $csvFileImporter = new CsvFileImporter($csvFile, new FileVacationCollection($codeOrg), $codeOrg, $year);
         try {
-            $csvFileImporter->run(); 
+            $csvFileImporter->run();
             $this->assertTrue(false);
-        }
-        catch(\Exception $exception) {
+        } catch (\Exception $exception) {
             $this->assertTrue(true);
         }
     }
@@ -48,14 +47,13 @@ class CsvFileImporterTest extends \Codeception\Test\Unit
     public function testLoadBadFileShort()
     {
         $codeOrg = '8600';
-        $year = '2024';
+        $year = date('Y');
         $csvFile = \Yii::getAlias('@app/tests/_files/import-data-bad-short.csv');
-        $csvFileImporter = new CsvFileImporter($csvFile, new SimpleVacationCollection($codeOrg), $codeOrg, $year);
+        $csvFileImporter = new CsvFileImporter($csvFile, new FileVacationCollection($codeOrg), $codeOrg, $year);
         try {
-            $csvFileImporter->run(); 
+            $csvFileImporter->run();
             $this->assertTrue(false);
-        }
-        catch(\Exception $exception) {
+        } catch (\Exception $exception) {
             $this->assertTrue(true);
         }
     }
