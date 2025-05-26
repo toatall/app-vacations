93dacfb17aa0a9dd15c6ea325ce498811dc5c63c
toatall <toatall@mail.ru>
2025-05-26 10:25:33 +0500

    Русифицирована страница входа (Login)

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -19,18 +19,18 @@ const login = () => {
   <Head title="Login" />
   <div class="flex items-center justify-center p-6 min-h-screen bg-indigo-800">
     <div class="w-full max-w-md">
-      <logo class="block mx-auto w-full max-w-xs fill-white" height="50" />
+      <logo class="block mx-auto w-full max-w-xs fill-white" style="width: 150px; height: 150px;" />
       <form class="mt-8 bg-white rounded-lg shadow-xl overflow-hidden" @submit.prevent="login">
         <div class="px-10 py-12">
           <h1 class="text-center text-3xl font-bold">Добро пожаловать!</h1>
           <div class="w-full">
             <div class="w-full mt-4">
               <text-input v-model="form.email" :error="form.errors.email" class="pb-8 pr-6 w-full lg:w-1/2"
-                label="Email or username" type="text" autofocus autocapitalize="off" />
+                label="Имя пользователя или email" type="text" autofocus autocapitalize="off" />
             </div>
             <div class="w-full mt-4">
               <text-input v-model="form.password" :error="form.errors.password" type="password"
-                class="pb-8 pr-6 w-full lg:w-1/2" label="Password" />
+                class="pb-8 pr-6 w-full lg:w-1/2" label="Пароль" />
             </div>
             <div class="w-full mt-4">
               <label class="flex items-center mt-6 select-none" for="remember">
@@ -41,7 +41,7 @@ const login = () => {
           </div>
         </div>
         <div class="flex px-10 py-4 bg-gray-100 border-t border-gray-100">
-          <loading-button :loading="form.processing" class="btn-indigo ml-auto" type="submit">Login</loading-button>
+          <loading-button :loading="form.processing" class="btn-indigo ml-auto" type="submit">Вход</loading-button>
         </div>
       </form>
     </div>
