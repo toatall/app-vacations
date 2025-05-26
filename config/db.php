91333bfac691f70c8f41ad7229d4029b32781970
toatall <toatall@mail.ru>
2025-05-26 16:15:12 +0500

    Обновлены данные для подключения к БД (для docker)

--- config/db.php
+++ config/db.php
@@ -2,12 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=app-vacations',
-    'username'=>'postgres',
-	'password'=>'P@ssw0rd',
+    'dsn' => 'pgsql:host=postgres;dbname=app-vacations',
+    'username' => 'vacation',
+    'password' => 'secret',
+    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
 ];
