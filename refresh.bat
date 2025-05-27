36eb685ff0509e90ff69bce2103074ed3cad594b
toatall <toatall@mail.ru>
2025-05-27 11:08:42 +0500

    Обновлены файлы обновления БД (refresh)

--- refresh.bat
+++ refresh.bat
@@ -7,14 +7,13 @@ php yii db/seed
 php yii roles/init
 php yii roles/assign "admin" "admin"
 
-php yii vacations/generate 100 8 runtime/vacations_0000.csv
-php yii csv-loader runtime/vacations_0000.csv 0000 %date:~6,4% ";"
+set "currentYear=%date:~-4%"
 
-php yii vacations/generate 80 8 runtime/vacations_0001.csv
-php yii csv-loader runtime/vacations_0001.csv 0001 %date:~6,4% ";"
-
-php yii vacations/generate 70 8 runtime/vacations_0002.csv
-php yii csv-loader runtime/vacations_0002.csv 0002 %date:~6,4% ";"
-
-php yii vacations/generate 90 10 runtime/vacations_0003.csv
-php yii csv-loader runtime/vacations_0003.csv 0003 %date:~6,4% ";"
+php yii vacations/generate 100 8 runtime\vacations_0000.csv
+php yii csv-loader runtime\vacations_0001.csv 0000 %currentYear% ";"
+php yii vacations/generate 80 8 runtime\vacations_0001.csv
+php yii csv-loader runtime\vacations_0001.csv 0001 %currentYear% ";"
+php yii vacations/generate 70 8 runtime\vacations_0002.csv
+php yii csv-loader runtime\vacations_0002.csv 0002 %currentYear% ";"
+php yii vacations/generate 90 10 runtime\vacations_0003.csv
+php yii csv-loader runtime\vacations_0003.csv 0003 %currentYear% ";"
--- refresh.sh
+++ refresh.sh
@@ -1,20 +1,22 @@
-php yii migrate/down --interactive=0 9
-php yii migrate/down --migrationPath=@yii/rbac/migrations --interactive=0 4
-php yii migrate --migrationPath=@yii/rbac/migrations --interactive=0
-php yii migrate --interactive=0
+while !</dev/tcp/postgres/5432; do sleep 1; done;
 
-php yii db/seed
-php yii roles/init
-php yii roles/assign "admin" "admin"
+php yii migrate/down --interactive=0 9;
+php yii migrate/down --migrationPath=@yii/rbac/migrations --interactive=0 4;
+php yii migrate --migrationPath=@yii/rbac/migrations --interactive=0;
+php yii migrate --interactive=0;
 
-php yii vacations/generate 100 8 runtime/vacations_0000.csv
-php yii csv-loader runtime/vacations_0001.csv 0000 $(date +%Y) ";"
+php yii db/seed;
+php yii roles/init;
+php yii roles/assign "admin" "admin";
 
-php yii vacations/generate 80 8 runtime/vacations_0001.csv
-php yii csv-loader runtime/vacations_0001.csv 0001 $(date +%Y) ";"
+php yii vacations/generate 100 8 runtime/vacations.csv;
+php yii csv-loader runtime/vacations.csv 0000 $(date +%Y) ";";
+php yii vacations/generate 80 8 runtime/vacations.csv;
+php yii csv-loader runtime/vacations.csv 0001 $(date +%Y) ";";
+php yii vacations/generate 70 8 runtime/vacations.csv;
+php yii csv-loader runtime/vacations.csv 0002 $(date +%Y) ";";
+php yii vacations/generate 90 10 runtime/vacations.csv;
+php yii csv-loader runtime/vacations.csv 0003 $(date +%Y) ";";
+rm runtime/vacations.csv;
 
-php yii vacations/generate 70 8 runtime/vacations_0002.csv
-php yii csv-loader runtime/vacations_0002.csv 0002 $(date +%Y) ";"
-
-php yii vacations/generate 90 10 runtime/vacations_0003.csv
-php yii csv-loader runtime/vacations_0003.csv 0003 $(date +%Y) ";"
+apache2-foreground
